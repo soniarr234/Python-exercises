49ddc19950848d301de6fa0d7441d17efbd39363
Sonia <104136134+soniarr234@users.noreply.github.com>
2026-02-13 11:10:21 +0100

    Create Guess the number.py

---
+++ Guess the number.py
@@ -0,0 +1,9 @@
+"""
+The program choose a number between 1 and 20 (you can use a fixed value initially).
+The user tries to guess it, and the program indicates wheter the number is higher or lower until they guess correctly.
+"""
+
+import random
+
+num_random = random.randint(1, 20)
+print(num_random)
