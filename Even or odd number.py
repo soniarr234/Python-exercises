87bdf3763ec04a5ea23800f833fec077eb4894f6
Sonia <104136134+soniarr234@users.noreply.github.com>
2026-02-13 10:52:05 +0100

    Create Even or odd number.py

---
+++ Even or odd number.py
@@ -0,0 +1,10 @@
+"""
+Ask the user for a number and say whether it is even or odd
+"""
+
+num = input ("Escribe un número")
+num int (num)
+if num 2 == 0:
+    print ("El número ", num, "es par")
+else:
+    print ("El número", num, "es impar")
