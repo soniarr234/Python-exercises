f30529afcf336006c7176265cf5961dd30fbf658
Sonia <104136134+soniarr234@users.noreply.github.com>
2026-02-13 10:33:11 +0100

    Create Decimal to binary.py

---
+++ Decimal to binary.py
@@ -0,0 +1,13 @@
+"""
+Create a program that transforms a decimal number into binary
+without using built-in language functions to do it directly.
+"""
+
+num = 34
+restos = []
+while num > 0:
+    num = num // 2
+    restos.append(num82)
+
+restos.reverse ()
+print (restos)
