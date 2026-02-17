820e24e7b228c42474bedb411d20e0b9f9dc8689
Sonia <104136134+soniarr234@users.noreply.github.com>
2026-02-17 12:59:50 +0100

    Create Possible pairs.py

---
+++ Possible pairs.py
@@ -0,0 +1,13 @@
+"""
+Given an array of numbers, print all possible pairs of numbers.
+And remove the duplicates, that is, (1,2) is the same as (2,1).
+Example:
+numbers = [1,2,3,4]
+pairs are (1,1),(1,2),(1,3),(1,4),(2,1)(2,2)... but you have to remove the duplicate
+"""
+
+numbers = [1,2,3,4]
+
+for i in range(len(numbers)):
+  for j in range(i+1, len(numbers)):
+    print (numbers[i], "-->", numbers[j])
