173cc992e8a5e9452f52506813ba4e41124a8580
Sonia <104136134+soniarr234@users.noreply.github.com>
2026-03-03 12:11:13 +0100

    Update common_elements.py

--- common_elements.py
+++ common_elements.py
@@ -8,16 +8,17 @@ def commonElements(array1, array2, bool):
         if array1[i] == array2[j]:
           if array2[j] not in common_num:
             common_num.append(array2[j])
+    return common_num
   else:
     #devolver los elementos no comundes
-    for i in range(len(array1)):
-      for j in range (len(array2)):
-        if array1[i] != array2[j]:
-          if array2[j] not in not_common_num:
-            not_common_num.append(array2[j])
-  print (common_num)
-  print (not_common_num)
+    for i in array1:
+      if i not in array2:
+        not_common_num.append(i)
 
+    for i in array2:
+      if i not in array1:
+        not_common_num.append(i)
+    return not_common_num
 
 array1 = [1, 2, 3, 3, 4]
 array2 = [2, 2, 3, 3, 3, 4, 6]
