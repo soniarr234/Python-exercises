f74c4b60de889bfbd9de6e119ad514c5f4e4eb94
Sonia <104136134+soniarr234@users.noreply.github.com>
2026-03-03 12:12:49 +0100

    Update common_elements.py

--- common_elements.py
+++ common_elements.py
@@ -1,3 +1,11 @@
+"""
+/*
+Crea una función que reciba dos array, un booleano y retorne un array.
+  - Si el booleano es verdadero buscará y retornará los elementos comunes de los dos array.
+  - Si el booleano es falso buscará y retornará los elementos no comunes de los dos array.
+  - No se pueden utilizar operaciones del lenguaje que lo resuelvan directamente.
+"""
+
 def commonElements(array1, array2, bool):
   common_num = []
   not_common_num = []
