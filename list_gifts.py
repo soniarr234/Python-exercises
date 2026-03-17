dd5f9f2c284d13d4695f7ecde8ab983db6c90d77
Sonia <104136134+soniarr234@users.noreply.github.com>
2026-03-17 15:19:29 +0100

    Update list_gifts.py

--- list_gifts.py
+++ list_gifts.py
@@ -1,3 +1,25 @@
+"""
+Te ha llegado una carta ✉️ con todos los regalos que debes preparar. El tema es que es una cadena de texto y es muy difícil de leer 😱. ¡Menos mal que han puesto cada regalo separado por espacio! (aunque ten cuidado, porque al ser niños, igual han colado más espacios de la cuenta)
+
+Encima nos hemos dado cuenta que algunas palabras vienen con un _ delante de la palabra, por ejemplo _playstation, que significa que está tachado y no se tiene que contar.
+
+Transforma el texto a un objeto que contenga el nombre de cada regalo y las veces que aparece. Por ejemplo, si tenemos el texto:
+
+const carta = 'bici coche balón _playstation bici coche peluche'
+Al ejecutar el método debería devolver lo siguiente:
+const regalos = listGifts(carta)
+console.log(regalos)
+/*
+{
+  bici: 2,
+  coche: 2,
+  balón: 1,
+  peluche: 1
+}
+*/
+Ten en cuenta que los tests pueden ser más exhaustivos... 😝 ¡Cuidado con contar espacios vacíos!
+"""
+
 def listGifts(letter):
   gifts = []
   gift = ""
